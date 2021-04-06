f89fb7f95a765e94c6f67a724093840a37d3725b
Ernesto Velasquez <ern.velasquez@alumnos.duoc.cl>
2021-04-05 23:46:45 -0400

    Trabajo terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //variables
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //create a function ramdom
+  function randomizar(param) {
+    var random = Math.floor(Math.random() * param.length);
+    return param[random];
+  }
+
+  //create a concat random excuse
+  let excuse =
+    randomizar(who) +
+    " " +
+    randomizar(action) +
+    " " +
+    randomizar(what) +
+    " " +
+    randomizar(when);
+
+  //change html text
+  document.getElementById("excuse").innerHTML = excuse;
 };
